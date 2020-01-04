e443ae15caa48ce47e9ec5b4fb6a5080cef5ddcc
Julian Lou <lou.julian96@gmail.com>
2020-01-03 20:46:52 -0800

    Update shebang

--- config/autostart-scripts/pactl.sh
+++ config/autostart-scripts/pactl.sh
@@ -1,4 +1,4 @@
-#!/bin/env/sh
+#!/bin/sh
 
 src=alsa_input.usb-Blue_Microphones_Yeti_Stereo_Microphone_FST_2018_05_18_95783-00.iec958-stereo
 snk=alsa_output.usb-NuForce_NuForce___DAC_2-01.analog-stereo
@@ -9,10 +9,10 @@ pactl set-default-sink   $snk
 
 # Unsuspend if necessary
 if pactl list sources short | grep $src | grep -q 'SUSPENDED'; then
-    pactl suspend-source $src 0
+    pactl suspend-source $src false
 fi
 
 if pactl list sinks short | grep $snk | grep -q 'SUSPENDED'; then
-    pactl suspend-sink $snk 0
+    pactl suspend-sink $snk false
 fi
 
